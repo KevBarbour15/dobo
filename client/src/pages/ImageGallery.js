b3b2274ba57566f60446d97ce1f83e7ad24715a3
Kevin Barbour <kbarbour@sfsu.edu>
2024-01-10 11:33:21 -0500

    Create ImageGallery.js
    Adding ImageGallery.js 
    
    (Corrupted repository)

---
+++ client/src/pages/ImageGallery.js
@@ -0,0 +1,33 @@
+import "./image-gallery.css";
+import { imagesArray } from "../../assets/images/photoArrays.js";
+import { Navigation, Pagination, EffectFade } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import 'swiper/css/effect-fade';
+
+const ImageGallery = () => {
+  return (
+    <div className="gallery-container">
+      <Swiper
+       effect={'fade'}
+        pagination={true}
+        navigation={true}
+       modules={[EffectFade, Pagination, Navigation]}
+      >
+        {imagesArray.map((image, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={image} alt="dobo" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageGallery;
