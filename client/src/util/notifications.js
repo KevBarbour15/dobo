fb4c9cd50c98aca48dfac084cbd55fb2a6a7ef33
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-02-05 20:17:28 -0500

    notification fix

--- client/src/util/notifications.js
+++ client/src/util/notifications.js
@@ -2,11 +2,11 @@ export const showSuccessNotification = (enqueueSnackbar, message) => {
   enqueueSnackbar(message, {
     autoHideDuration: 1250,
     style: {
-      backgroundColor: "rgba(255, 255, 255, 0.8)",
+      backgroundColor: "efefef",
       color: "#bd9c72",
-      border: "2pxsolid #bd9c72",
+      border: "2px solid #bd9c72",
       borderRadius: "5px",
-      fontSize: "20px",
+      fontSize: "14px",
       fontFamily: "Poppins, sans-serif",
       fontWeight: "bold",
       textTransform: "lowercase",
