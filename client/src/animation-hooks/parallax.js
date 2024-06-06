8d822346949200b0e0130163b578fc6fa2bdce11
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-06-06 14:42:54 -0400

    fixed title page first letter disappearing bug

--- client/src/animation-hooks/parallax.js
+++ client/src/animation-hooks/parallax.js
@@ -1,10 +1,12 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const useParallax = () => {
   useGSAP(() => {
-    // parallax scroll animation
+    gsap.registerPlugin(ScrollTrigger, SplitText);
+
     let titleSplitUpper = new SplitText(".page-title", {
       type: "chars",
     });
@@ -13,67 +15,69 @@ const useParallax = () => {
       type: "chars",
     });
 
-    let scrollTl = gsap.timeline();
-
-    scrollTl
-      .from(
-        titleSplitUpper.chars,
-        {
-          opacity: 0,
-          stagger: 0.15,
-          x: function (i) {
-            return i % 2 === 0 ? 250 : -250;
-          },
-        },
-        0
-      )
-      .to(
-        ".image-layer",
-        {
-          opacity: 0,
+    // Initial animation for titleSplitUpper on page load
+    gsap.from(titleSplitUpper.chars, {
+      opacity: 0,
+      stagger: 0.1,
+      duration: 1,
+      x: (i) => (i % 2 === 0 ? 250 : -250),
+      onComplete: () => {
+        let scrollTl = gsap.timeline({
+          ease: "linear",
           scrollTrigger: {
-            scrub: 1,
-            trigger: ".image-layer",
-            start: "center center",
-            end: "+=850",
-          },
-        },
-        0
-      )
-      .to(
-        titleSplitUpper.chars,
-        {
-          x: function (i) {
-            return i % 2 === 0 ? 250 : -250;
-          },
-          opacity: 0,
-          stagger: 0.15,
-          scrollTrigger: {
-            scrub: 3,
             trigger: ".page-title",
             start: "center center-=100",
             end: "+=100",
+            scrub: 1,
           },
-        },
-        0
-      )
-      .from(
-        titleSplitLower.chars,
-        {
-          opacity: 0,
-          stagger: 0.15,
-          x: function (i) {
-            return i % 2 === 0 ? 250 : -250;
-          },
-          scrollTrigger: {
-            trigger: ".title-container",
-            start: "top center",
-            end: "+=150",
-            scrub: 3,
-          },
-        },
-        0
-      );
+        });
+
+        scrollTl
+          .to(
+            ".image-layer",
+            {
+              opacity: 0,
+              scrollTrigger: {
+                scrub: 1,
+                trigger: ".image-layer",
+                start: "center center",
+                end: "+=850",
+              },
+            },
+            0
+          )
+          .to(
+            titleSplitUpper.chars,
+            {
+              x: (i) => (i % 2 === 0 ? 250 : -250),
+              opacity: 0,
+              stagger: 0.15,
+              scrollTrigger: {
+                scrub: 2,
+                trigger: ".page-title",
+                start: "center center-=100",
+                end: "+=100",
+              },
+            },
+            0
+          )
+          .from(
+            titleSplitLower.chars,
+            {
+              opacity: 0,
+              stagger: 0.15,
+              x: (i) => (i % 2 === 0 ? 250 : -250),
+              scrollTrigger: {
+                trigger: ".title-container",
+                start: "top center",
+                end: "+=150",
+                scrub: 2,
+              },
+            },
+            0
+          );
+      },
+    });
   });
 };
 
