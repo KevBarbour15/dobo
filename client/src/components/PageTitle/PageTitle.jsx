dd681fc9d6b1fce2ba2941cde7fc19d5a5418acc
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2025-01-14 14:45:52 -0500

    scroll effect update

--- client/src/components/PageTitle/PageTitle.jsx
+++ client/src/components/PageTitle/PageTitle.jsx
@@ -4,6 +4,7 @@ const PageTitle = ({ title }) => {
   return (
     <div className="title-container">
       <h2 className="title">{title}</h2>
+      <h2 className="title-dark">{title}</h2>
     </div>
   );
 };
